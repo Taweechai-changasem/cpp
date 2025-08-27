e89ec25ccf889317658d44633e85c078bb0f498d
Taweechai-changasem <68219010011@udontech.ac.th>
2025-08-27 11:27:18 +0700

    Create ODD.cpp

---
+++ ODD.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+int loop_num = 1;
+int fixed_loop;
+
+void getLoop(){
+    std::cout << "Enter number of loop : ";
+    std::cin >> fixed_loop;
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cout << "===ODD Program===" << std::endl;
+    getLoop();
+    while(loop_num <= fixed_loop){
+        if(loop_num % 2 == 0){
+         std::cout << "Number : " << loop_num << std::endl;
+        }
+        loop_num++;
+    }
+
+    return 0;
+}
